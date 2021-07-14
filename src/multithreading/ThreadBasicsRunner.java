f453b51f545d239de9fa2c2b63e20fa7acfda75c
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-07-14 22:37:09 +0700

    end of 243: creating a thread for task 1 - extending Thread class

--- src/multithreading/ThreadBasicsRunner.java
+++ src/multithreading/ThreadBasicsRunner.java
@@ -9,6 +9,22 @@ package multithreading;
  *
  * @author huynq
  */
+class Task1 extends Thread {
+
+    @Override
+    public void run() {
+
+        System.out.println("Task1 started...");
+
+        // Task 1
+        for (int i = 101; i <= 199; ++i) {
+            System.out.print(i + " ");
+        }
+        System.out.print("\nTask 1 Done");
+
+    }
+}
+
 public class ThreadBasicsRunner {
 
     /**
@@ -18,18 +34,20 @@ public class ThreadBasicsRunner {
         System.out.println("Multi Threading");
 
         // Task 1
-        for (int i = 101; i <= 199; ++i) {
-            System.out.print(i + " ");
-        }
-        System.out.print("\nTask 1 Done");
+        System.out.println("Task 1 kicked off");
+        Task1 task1 = new Task1();
+        //task1.run();
+        task1.start();
 
         // Task 2
+        System.out.println("Task 2 kicked off");
         for (int i = 201; i <= 299; ++i) {
             System.out.print(i + " ");
         }
         System.out.print("\nTask 2 Done");
 
         // Task 3
+        System.out.println("Task 3 kicked off");
         for (int i = 301; i <= 399; ++i) {
             System.out.print(i + " ");
         }

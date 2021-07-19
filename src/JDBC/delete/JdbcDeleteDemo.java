14d1d88c9bb0d11c71a7f20353e1724c1da8f301
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-07-19 09:15:06 +0700

    12: delete data from database

---
+++ src/JDBC/delete/JdbcDeleteDemo.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JDBC.delete;
+
+import JDBC.DatabaseConnectionManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author huynq
+ */
+public class JdbcDeleteDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Connection myConn = null;
+        Statement myStmt = null;
+        ResultSet myRs = null;
+
+        DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost:3306", "demo", "admin", "123456");
+
+        try {
+
+            // get a connection to database
+            myConn = dcm.getConnection();
+            System.out.println("Database connection successful!");
+
+            // create a statement
+            myStmt = myConn.createStatement();
+
+            // execute sql query: insert row
+            String sql = """
+                         delete from employees
+                         where id = 13
+                         """;
+            int rowsAffected = myStmt.executeUpdate(sql);
+
+            // process the resultset
+            myRs = myStmt.executeQuery("select * from employees");
+
+            System.out.println("Rows affected: " + rowsAffected);
+            while (myRs.next()) {
+                System.out.println(myRs.getString("last_name") + ", " + myRs.getString("first_name"));
+            }
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+
+        } finally {
+
+            if (myRs != null) try {
+                myRs.close();
+            } catch (SQLException ignore) {
+            }
+            if (myStmt != null) try {
+                myStmt.close();
+            } catch (SQLException ignore) {
+            }
+            if (myConn != null) try {
+                myConn.close();
+            } catch (SQLException ignore) {
+            }
+
+        }
+    }
+
+}

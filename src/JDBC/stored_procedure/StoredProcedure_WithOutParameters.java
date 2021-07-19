74d5f507e34df0c1da90de017928e592ac363cb0
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-07-19 17:12:40 +0700

    14, 15, 16, 17: Calling Stored Procedures

---
+++ src/JDBC/stored_procedure/StoredProcedure_WithOutParameters.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JDBC.stored_procedure;
+
+import JDBC.DatabaseConnectionManager;
+import java.sql.Connection;
+import java.sql.CallableStatement;
+//import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+
+/**
+ *
+ * @author huynq
+ */
+public class StoredProcedure_WithOutParameters {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        DatabaseConnectionManager dcm
+                = new DatabaseConnectionManager("localhost:3306", "demo", "admin", "123456");
+
+        Connection myConn = null;
+        CallableStatement myStmt = null;
+//        ResultSet myRs = null;
+
+        try {
+
+            // get connection
+            myConn = dcm.getConnection();
+
+            // create callable statement
+            myStmt = myConn.prepareCall("{ call get_count_for_department(?, ?) }");
+
+            // set parameters
+            String department = "Engineering";
+            myStmt.setString(1, department);
+            myStmt.registerOutParameter(2, Types.INTEGER);
+
+            // call statement
+            myStmt.execute();
+
+            // get the value of OUT parameter
+            int count = myStmt.getInt(2);
+            System.out.println("Count: " + count);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+
+//            if (myRs != null) try {
+//                myRs.close();
+//            } catch (SQLException ignore) {
+//            }
+            if (myStmt != null) try {
+                myStmt.close();
+            } catch (SQLException ignore) {
+            }
+            if (myConn != null) try {
+                myConn.close();
+            } catch (SQLException ignore) {
+            }
+
+        }
+
+    }
+
+}

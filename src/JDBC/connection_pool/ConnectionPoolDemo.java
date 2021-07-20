63ea0a596859e428cd2e5230fb6e545c6f975978
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2021-07-20 22:42:46 +0700

    connection pooling - not work??? - need maven?

---
+++ src/JDBC/connection_pool/ConnectionPoolDemo.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JDBC.connection_pool;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author huynq
+ */
+public class ConnectionPoolDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+
+            connection = DatabaseConnectionPoolManager.getConnection();
+            statement = connection.createStatement();
+            String sql = "SELECT * FROM employees";
+            resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("first_name"));
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+
+            if (resultSet != null) try {
+                resultSet.close();
+            } catch (SQLException ignore) {
+            }
+            if (statement != null) try {
+                statement.close();
+            } catch (SQLException ignore) {
+            }
+            if (connection != null) try {
+                connection.close();
+            } catch (SQLException ignore) {
+            }
+
+        }
+    }
+
+}
